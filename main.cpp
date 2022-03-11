c0285321f03f6d2cbc8c84387a31748a5aab6afc
yongan xiang <742035327@qq.com>
2022-03-11 15:02:05 +0800

    solve crash

--- main.cpp
+++ main.cpp
@@ -7,4 +7,5 @@ int main() {
     //BFS bfs = BFS("data/10kV_100kE.txt", "env/kernel/BFS_GPU.cl", 0,0,2);
     //Kruskal kruskal = Kruskal("data/10kV_100kE.txt", "env/kernel/Kruskal_GPU.cl", 0,0,2);
     WCC wcc = WCC("../data/10kV_100kE.txt", "../env/kernel/WCC_GPU.cl",0,2);
+
 }
